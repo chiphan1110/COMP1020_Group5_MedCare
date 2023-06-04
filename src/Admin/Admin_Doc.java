912414eac4458057f69c925343be632ff60d41b7
sangf82 <sluongtran@gmail.com>
2023-06-04 11:34:46 +0700

    update doctor name in FinalPostResult

--- src/Admin/Admin_Doc.java
+++ src/Admin/Admin_Doc.java
@@ -8,12 +8,4 @@ package Admin;
  *
  * @author Admin
  */
-public class Admin_Doc {
-    private byte[] pictures;
-    public Admin_Doc(byte[] image){
-        this.pictures = image;
-    }
-    public byte[] getImage() {
-        return pictures;
-    }
-}
+
